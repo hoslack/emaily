08b1f89e3c87b01cf73fd54ab9813c383cf81c70
hoslack <hoslackochieng@gmail.com>
2017-12-24 18:30:42 +0200

    complete auth

--- index.js
+++ index.js
@@ -1,36 +1,24 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./models/User');
+require('./services/passport');
+
 const app = express();
 
-passport.use(
-	new GoogleStrategy(
-		{
-			clientID: keys.googleClientID,
-			clientSecret: keys.googleClientSecret,
-			callbackURL: '/auth/google/callback',
-		},
-		(accessToken, refreshToken, profile, done) => {
-			console.log('accessToken', accessToken);
-			console.log('refreshToken', refreshToken);
-			console.log('profile', profile);
-		}
-	)
-);
-
-app.get('/', (req, res) => {
-	res.send({ hi: 'there' });
-});
-
-app.get(
-	'/auth/google',
-	passport.authenticate('google', {
-		scope: ['profile', 'email'],
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookieKey],
 	})
 );
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/auth/google/callback', passport.authenticate('google'));
+mongoose.connect(keys.mongoURI);
+require('./routes/authRoutes')(app);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {

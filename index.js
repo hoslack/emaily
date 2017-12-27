638c887aaa4b4bd199fde06935697aa5ff694e84
hoslack <hoslackochieng@gmail.com>
2017-12-27 11:03:14 +0200

    corrected accidental delete of index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./models/User');
+require('./services/passport');
+
+const app = express();
+
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookieKey],
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect(keys.mongoURI);
+require('./routes/authRoutes')(app);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+	console.log('app listening on ', port);
+});
